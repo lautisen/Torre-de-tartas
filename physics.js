09331d6d18150067443c69014bd6b0c3de04906b
lautisen <bocalauti@gmail.com>
2026-02-18 23:39:48 +0100

    Update physics.js

--- physics.js
+++ physics.js
@@ -1,6 +1,7 @@
 const physics = {
     calculateOffset(x, width) {
-        // Centro de pantalla - Centro de tarta
-        return (x + (width / 2)) - (window.innerWidth / 2);
+        const screenCenter = window.innerWidth / 2;
+        const cakeCenter = x + (width / 2);
+        return cakeCenter - screenCenter;
     }
 };
