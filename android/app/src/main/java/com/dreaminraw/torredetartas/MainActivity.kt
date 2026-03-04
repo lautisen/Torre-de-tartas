547b6da8406d20258519eb7c1d497363f894ff4c
lautisen <bocalauti@gmail.com>
2026-03-05 00:16:10 +0100

    feat: initial Android wrapping project setup (dreaminraw publisher)

---
+++ android/app/src/main/java/com/dreaminraw/torredetartas/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.dreaminraw.torredetartas
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+        
+        // Configure WebView
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.mediaPlaybackRequiresUserGesture = false // Allow audio/video to play without requiring user tap
+        
+        webView.webViewClient = WebViewClient() // Keeps navigation inside the WebView
+        webView.webChromeClient = WebChromeClient() // Helps with JS alerts/console
+        
+        webView.loadUrl("https://cake-game.online/game.html")
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
